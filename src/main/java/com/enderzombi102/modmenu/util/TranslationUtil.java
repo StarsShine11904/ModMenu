cb2f5b70fd459a1c2e6fb0d5d602c2cb38157681
StarsShine11904 <89504980+StarsShine11904@users.noreply.github.com>
2026-09-01 13:11:00 +0800

    Change translation to use LiteralText
    Refactor translation key handling to use LiteralText instead of TranslatableText.

--- src/main/java/com/enderzombi102/modmenu/util/TranslationUtil.java
+++ src/main/java/com/enderzombi102/modmenu/util/TranslationUtil.java
@@ -2,8 +2,8 @@ package com.enderzombi102.modmenu.util;
 
 import com.enderzombi102.modmenu.ModMenu;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 
 import java.text.NumberFormat;
 import java.util.Arrays;
@@ -33,7 +33,7 @@ public class TranslationUtil {
 			}
 		}
 
-		String lastKey = key;
+		String chosenKey = key;
 		for ( int flags = ( 1 << args.length ) - 1; flags >= 0; flags-- ) {
 			StringBuilder fullKey = new StringBuilder( key );
 			for ( int i = 0; i < args.length; i++ ) {
@@ -44,12 +44,15 @@ public class TranslationUtil {
 					fullKey.append( 'n' );
 				}
 			}
-			lastKey = fullKey.toString();
-			if ( I18n.method_12500( lastKey ) ) {
-				return new TranslatableText( lastKey, realArgs );
+			String candidateKey = fullKey.toString();
+			if ( I18n.method_12500( candidateKey ) ) {
+				chosenKey = candidateKey;
+				break;
 			}
 		}
-		return new TranslatableText( lastKey, realArgs );
+
+		// 使用 I18n.translate 格式化文字並包裝為 LiteralText 返回
+		return new LiteralText( I18n.translate( chosenKey, realArgs ) );
 	}
 
 	public static String translationKeyOf( String type, String id ) {
